922a776819e95bce578ba35fdccc0e61f85914e1
elevate-kyle <kyle@elevateclub.org>
2024-02-03 14:31:46 -0500

    cool functions

--- 2024-02-03/index.js
+++ 2024-02-03/index.js
@@ -4,12 +4,20 @@ var ox = 0;
 var oy = 0;
 var pox = ox+50;
 var poy = oy+60;
-ctx.beginPath();
-//frame
-ctx.rect(ox, oy, 100, 200);
-//tmino
-ctx.rect(pox, poy, 10, 10);
-ctx.rect(pox+10, poy, 10, 10);
-ctx.rect(pox+20, poy, 10, 10);
-ctx.rect(pox+10, poy+10, 10, 10);
-ctx.stroke();
+
+function frame(){
+    ctx.beginPath();
+    ctx.rect(ox, oy, 100, 200);
+    ctx.stroke();
+}
+
+function tmino(pox, poy){
+    ctx.beginPath();
+    ctx.rect(pox, poy, 10, 10);
+    ctx.rect(pox+10, poy, 10, 10);
+    ctx.rect(pox+20, poy, 10, 10);
+    ctx.rect(pox+10, poy+10, 10, 10);
+    ctx.stroke();
+}
+frame(10, 10)
+tmino(10, 10)
