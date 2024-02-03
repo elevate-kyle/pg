a8d1934075c5722f5b1c3295f034267bfdb1cbef
elevate-kyle <kyle@elevateclub.org>
2024-02-03 12:43:38 -0500

    tmino

--- 2024-02-03/index.js
+++ 2024-02-03/index.js
@@ -2,4 +2,8 @@ var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.rect(100, 100, 100, 200);
+ctx.rect(150, 150, 10, 10);
+ctx.rect(160, 150, 10 , 10);
+ctx.rect(170, 150, 10, 10);
+ctx.rect(160, 160, 10, 10)
 ctx.stroke();
