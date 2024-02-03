49b6944fa400db150e89834286702d454e3f64a9
elevate-kyle <kyle@elevateclub.org>
2024-02-03 12:38:03 -0500

    rectangle

--- 2024-02-03/index.js
+++ 2024-02-03/index.js
@@ -1,2 +1,5 @@
-var sh = "helloworld"
-console.log(sh)
+var canvas = document.querySelector("#canvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(100, 100, 100, 200);
+ctx.stroke();
